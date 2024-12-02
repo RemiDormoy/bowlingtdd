f6cd8e76c7a913017057387d77657f41bb106354
Enzo Bassot <61282740+MrzozoGit@users.noreply.github.com>
2024-12-02 18:34:37 +0900

    premier test

--- bowling.js
+++ bowling.js
@@ -5,11 +5,18 @@
 
 export class Game {
 
+
+    constructor() {
+        this.score = 0;
+    }
+
     lancerDeBoule(nbQuilles) {
+
         throw new Error('Remove this statement and implement this function');
     }
 
     get score() {
+        return this.score;
         throw new Error('Remove this statement and implement this function');
     }
 }
